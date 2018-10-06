919c29d500f159dfc9441889a74f7cd30a70e238
L00149524 <41998910+L00149524@users.noreply.github.com>
2018-10-07 00:14:09 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Travis-CI';
+  title = 'Travis-CI';  
+  var err = "injected error";
 }
